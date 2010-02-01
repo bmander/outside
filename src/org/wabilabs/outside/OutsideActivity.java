27553906f3c7c00c1f17e57f6c602d3f0b2ad070
Brandon Martin-Anderson <badhill@gmail.com>
2010-02-01 14:58:29 -0800

    basic android app stub

---
+++ src/org/wabilabs/outside/OutsideActivity.java
@@ -0,0 +1,15 @@
+package org.wabilabs.outside;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OutsideActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
